d377c0b9e36fa193b8f32c49a41ca3f28ad06199
GrishinViacheslav <40839049+GrishinViacheslav@users.noreply.github.com>
2024-07-26 20:23:18 +0300

    Add files via upload

--- krovato/js/script.js
+++ krovato/js/script.js
@@ -1,1 +1,32 @@
-"use strict";
+// Строгий режим
+"use strict"
+
+const maxWidth = +document.querySelector('.menu-footer').dataset.spollersInit || 600
+
+let documentActions = (e) => {
+	const targetElement = e.target
+
+	if (targetElement.closest('.menu-footer__title')) {
+		if (window.innerWidth > maxWidth) {
+			e.preventDefault()
+		}
+	}
+}
+let spollersInit = (footerSpollers, isOpen) => {
+	footerSpollers.forEach(footerSpoller => {
+		footerSpoller.classList.toggle('_init', !isOpen)
+		footerSpoller.open = isOpen
+	})
+}
+document.addEventListener("click", documentActions)
+
+// Footer Spollers
+const footerSpollers = document.querySelectorAll('.menu-footer__item')
+if (footerSpollers.length) {
+	const matchMedia = window.matchMedia(`(max-width: ${maxWidth}px)`)
+	spollersInit(footerSpollers, !matchMedia.matches)
+	matchMedia.addEventListener('change', () => {
+		spollersInit(footerSpollers, !matchMedia.matches)
+	})
+}
+
